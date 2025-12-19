9fcaf63c796842238bb1c6377b1ec5d97c661d73
bigxage <haixage@gmail.com>
2025-12-20 00:17:47 +0100

    more

---
+++ src/bin/p5.rs
@@ -0,0 +1,13 @@
+//fill the blank to make it work
+fn main() {
+    let x: f64 = 1_000.000_1; // ?
+    let _y: f32 = 0.12; // f32
+    let _z: f64 = 0.01_f64; // f64
+
+    assert_eq!(type_of(&x), "f64".to_string());
+    println!("success!");
+}
+
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
